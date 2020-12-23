b9e97598dc0bd0bf7f3039a587c68acb2a4cb747
Joe Reddington <joe@joereddington.com>
2020-12-23 13:02:43 +0000

    now showing a screenshot

--- go.py
+++ go.py
@@ -3,11 +3,11 @@ import pyautogui
 from PIL import ImageTk, Image
 
 
-def show_imge(path):
+def show_imge():
     image_window = tk.Tk()
-    img = ImageTk.PhotoImage(Image.open(path))
+    img = ImageTk.PhotoImage(pyautogui.screenshot())
     panel = tk.Label(image_window, image=img)
     panel.pack(side="bottom", fill="both", expand="yes")
     image_window.mainloop()
 
-show_imge('test.png')
+show_imge() 
