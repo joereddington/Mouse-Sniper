1173bfac1cffedad14adaf7592a926acfc75b428
Joe Reddington <joe@joereddington.com>
2020-12-23 13:47:17 +0000

    We have image displayed and basic keyboard input

--- go.py
+++ go.py
@@ -5,9 +5,21 @@ from PIL import ImageTk, Image
 
 def show_imge():
     image_window = tk.Tk()
-    img = ImageTk.PhotoImage(pyautogui.screenshot())
+    image_window.geometry("400x225")
+    img = pyautogui.screenshot()
+    img = img.resize((400, 225), Image.ANTIALIAS)
+    img = ImageTk.PhotoImage(img)
     panel = tk.Label(image_window, image=img)
     panel.pack(side="bottom", fill="both", expand="yes")
+    image_window.after(1000,keyboard_stuff)
     image_window.mainloop()
 
+
+
+def keyboard_stuff():
+    import keyboard 
+    while True: 
+       if keyboard.read_key() == "p":
+            print("you pressed p") 
+            continue
 show_imge() 
