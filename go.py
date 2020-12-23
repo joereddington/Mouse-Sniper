1b2cdcf0c9034e866901363c504e79e499232404
Joe Reddington <joe@joereddington.com>
2020-12-23 11:55:51 +0000

    displays image

---
+++ go.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+import pyautogui
+from PIL import ImageTk, Image
+
+
+def show_imge(path):
+    image_window = tk.Tk()
+    img = ImageTk.PhotoImage(Image.open(path))
+    panel = tk.Label(image_window, image=img)
+    panel.pack(side="bottom", fill="both", expand="yes")
+    image_window.mainloop()
+
+show_imge('test.png')
