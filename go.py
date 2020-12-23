9ed6403efb01e62bf3a9d13b35bb68aba5d7bca0
Joe Reddington <joe@joereddington.com>
2020-12-23 20:59:54 +0000

    I can zoom in once, but only once...

--- go.py
+++ go.py
@@ -2,8 +2,13 @@ import tkinter as tk
 import pyautogui
 from PIL import ImageTk, Image
 
+minx=0
+miny=0
+(maxx,maxy)=pyautogui.size()
+
 def get_screenshot():
-    img = pyautogui.screenshot()
+    print(minx,miny,maxx,maxy)
+    img = pyautogui.screenshot(region=(minx,miny,maxx,maxy))
     img = img.resize((400, 225), Image.ANTIALIAS)
     img = ImageTk.PhotoImage(img)
     return img
@@ -20,7 +25,18 @@ def callback(e):
     panel.configure(image=img2)
     panel.image = img2
 
+def zoom(e): 
+    print("five was pressed") 
+    global minx,maxx,miny,maxy
+    new_minx=minx+((maxx-minx)/3)
+    new_miny=miny+((maxy-miny)/3)
+    new_maxx=maxx-((maxx-minx)/3)
+    new_maxy=maxy-((maxy-miny)/3)
+    (minx,miny,maxx,maxy)= (new_minx,new_miny,new_maxx,new_maxy)
+    callback(e)
+
 root.bind("<Return>", callback)
+root.bind("5", zoom)
 root.mainloop()
 
 
