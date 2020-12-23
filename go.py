13a7d0dbede11396e48babbe66e3e81fcf42fa4f
Joe Reddington <joe@joereddington.com>
2020-12-23 20:20:48 +0000

    should work

--- go.py
+++ go.py
@@ -1,25 +1,29 @@
 import tkinter as tk
+import keyboard 
 import pyautogui
 from PIL import ImageTk, Image
 
+image_window = tk.Tk()
 
-def show_imge():
-    image_window = tk.Tk()
-    image_window.geometry("400x225")
+def get_screenshot():
     img = pyautogui.screenshot()
     img = img.resize((400, 225), Image.ANTIALIAS)
     img = ImageTk.PhotoImage(img)
-    panel = tk.Label(image_window, image=img)
+    return img
+
+def show_imge():
+    image_window.geometry("400x225")
+    panel = tk.Label(image_window, image=get_screenshot())
     panel.pack(side="bottom", fill="both", expand="yes")
     image_window.after(1000,keyboard_stuff)
     image_window.mainloop()
 
-
-
 def keyboard_stuff():
-    import keyboard 
     while True: 
        if keyboard.read_key() == "p":
             print("you pressed p") 
+            panel = tk.Label(image_window, image=get_screenshot())
+            panel.pack(side="bottom", fill="both", expand="yes")
             continue
+
 show_imge() 
