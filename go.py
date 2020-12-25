8422faa40d6cc03ddc3821aef7120a652e3ff071
Joe Reddington <joe@joereddington.com>
2020-12-25 22:24:05 +0000

    I've sped up the preview window refresh by croping and image rather than taking a new one and I've introduced a zoom limiter, but I'm confused by a bug that appears if I do 4/6 followed by any of 789 - the screen white's out...

--- go.py
+++ go.py
@@ -5,6 +5,8 @@ from PIL import ImageTk, Image
 minx=0
 miny=0
 (maxx,maxy)=pyautogui.size()
+ori_img=0
+
 
 def move_mouse(): 
 #moves mouse to the middle of the x's and y's 
@@ -13,23 +15,29 @@ def move_mouse():
     pyautogui.moveTo(x,y)
 
 def get_screenshot():
-    img = pyautogui.screenshot(region=(minx,miny,maxx-minx,maxy-miny))#because the last two are heigh and width
+    global ori_img
+    ori_img = pyautogui.screenshot()
+    img = ori_img.resize((400, 225), Image.ANTIALIAS)
+    img = ImageTk.PhotoImage(img)
+    return img
+
+
+def update_image():
+    global ori_img
+    global minx,maxx,miny,maxy
+    (height,width)=pyautogui.size()
+    img=ori_img.crop((minx,miny,maxx,maxy))
     img = img.resize((400, 225), Image.ANTIALIAS)
     img = ImageTk.PhotoImage(img)
     return img
 
-root = tk.Tk()
-root.geometry("400x225")
-
-img = get_screenshot()
-panel = tk.Label(root, image=img)
-panel.pack(side="bottom", fill="both", expand="yes")
 
 def update_screen(e):
     move_mouse()
-    img2 = get_screenshot()
+    img2 = update_image()
     panel.configure(image=img2)
     panel.image = img2
+
 
 def reset(e):
     global minx,maxx,miny,maxy
@@ -37,22 +45,28 @@ def reset(e):
     miny=0
     (maxx,maxy)=pyautogui.size()
     update_screen(e)
+    get_screenshot()#refreshes the ori_img
     
-
 def key_pressed(e):
     global minx,maxx,miny,maxy
     height=maxy-miny
-    new_height=height/3
+    new_height=int(height/3)
     width=maxx-minx
-    new_width=width/3
-    print(e.char)
+    new_width=int(width/3)
+    if new_width<3:
+        print("Don't want to zoom more")
+        return
+    if new_height<3:
+        print("Don't want to zoom more")
+        return
+    print("{}: {},{},{},{}".format(e.char,minx,maxx,miny,maxy))
     if (e.char in "123"):
         miny=miny+new_height+new_height
     if (e.char in "456"):
         miny=miny+new_height
         maxy=maxy-new_height
     if (e.char in "789"):
-        maxy=maxy/3
+        maxy=int(maxy/3)
     if (e.char in "741"):
         maxx=minx+new_width
     if (e.char in "852"):
@@ -62,13 +76,19 @@ def key_pressed(e):
         minx=minx+((maxx-minx)/3*2)
     update_screen(e)
 
-
 def click(e): 
     print("clicked") 
     pyautogui.click()
     pyautogui.click()#clicking twice because the first makes the target application active. 
+    get_screenshot()#refreshes the ori_img
 
-root.bind("<Return>", click)
+
+root = tk.Tk()
+root.geometry("400x225")
+img = get_screenshot()
+panel = tk.Label(root, image=img)
+panel.pack(side="bottom", fill="both", expand="yes")
+root.bind("<KP_Enter>", click)
 root.bind("1", key_pressed)
 root.bind("2", key_pressed)
 root.bind("3", key_pressed)
